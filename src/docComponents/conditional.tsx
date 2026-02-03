dd25f547511a435ccdaca685aa884c0fb25b87a8
Juvenn Woo <wuyun@xd.com>
2026-02-03 16:31:47 +0800

    chore: add tencent cloudbase #no-ticket (#17)

--- src/docComponents/conditional.tsx
+++ src/docComponents/conditional.tsx
@@ -1,23 +1,21 @@
 import { BRAND, REGION } from "../constants/env";
+import React, { type ReactNode } from "react";
 
 interface ConditionalProps {
-  children: React.ReactElement[];
+  children?: ReactNode;
   brand?: string;
   region?: string;
   if?: boolean;
 }
 
 export function Conditional(props: ConditionalProps) {
-  
-  
-
   const { children, brand, region } = props;
   if (
     (!brand || brand === BRAND) &&
     (!region || region === REGION) &&
     (props.if === undefined || props.if)
   ) {
-    return children;
+    return <>{children}</>;
   } else {
     return null;
   }
